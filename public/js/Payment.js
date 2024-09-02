58f5f47b2850f714710813166d509b7cb9863a94
Gnarendra <dev.narendra@icloud.com>
2024-09-02 23:32:54 +0530

    Layout changes

--- public/js/Payment.js
+++ public/js/Payment.js
@@ -1,23 +1,14 @@
 const sessionId = localStorage.getItem('session')
-
-function getResponsiveDimensions() {
-   const width = window.innerWidth <= 425 ? "100%" : "425px";
-   const height = window.innerHeight <= 700 ? "100%" : "700px";
-   
-   return { width, height };
-}
-
 if(sessionId){
    const cashfree = Cashfree({
       mode: "sandbox",
   });
-  const dimensions = getResponsiveDimensions();
    let checkoutOptions = {
       paymentSessionId: sessionId,
       redirectTarget: document.getElementById("cf_checkout"),
       appearance: {
-         width: dimensions.width,
-         height: dimensions.height,
+         width: window.innerWidth <= 425 ? "100%" : "425px",
+         height: window.innerHeight <= 700 ? "100%" : "700px",
      },
    };
    cashfree.checkout(checkoutOptions).then((result) => {
@@ -32,8 +23,7 @@ if(sessionId){
           console.log("Payment has been completed, Check for Payment Status");
           console.log(result.paymentDetails.paymentMessage);
       }
-   }).finally(()=>{
-      window.length.href = 'https://cashfree.onrender.com'
-      localStorage.clear()
-   });
+   })
+   window.length.href = 'https://cashfree.onrender.com'
+   localStorage.clear()
 }
